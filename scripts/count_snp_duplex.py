63237681cdce780f20d9c2f59b053377cc16ef40
Бабин Юрий Юрьевич <babin@pcr.int>
2019-01-23 13:40:48 +0300

    script editing

--- scripts/count_snp_duplex.py
+++ scripts/count_snp_duplex.py
@@ -35,33 +35,42 @@ NOTE: to speed up the exection instead of 'SeqIO.parse()
 low level fasta parser 'SimpleFastaParser' is used 
 """
 
-
-import os
 import datetime
+import os
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import numpy as np
-from time import time
+import pandas as pd
+import seaborn as sns
 from Bio.SeqIO.FastaIO import SimpleFastaParser  # low level fast fasta parser
 from ipywidgets import FloatProgress
 from IPython.display import display
+from time import time
 sns.set()
+
+
+def get_input_files_names(path_to_input):
+    """returns a list of input fasta files
+    """
+    fasta_extensions = ["fasta", "fa", "fas"]
+    input_files = os.listdir(path_to_input)
+    input_files = [f for f in input_files if f.rsplit(
+        ".", 1)[-1] in fasta_extensions]
+
+    return input_files
 
 
 def get_ref(in_file):
     """deriving the first record 
     from the input fasta
     """
-    
+
     with open("./input_data/" + in_file) as in_handle:
         for record in SimpleFastaParser(in_handle):
             # sequence is under the 2nd index in the record's tuple
-            ref_seq = record[1] 
+            ref_seq = record[1]
             break
-    
+
     return ref_seq
-    
 
 
 def get_duplex_posits(ref_seq, ref_nuc):
@@ -69,78 +78,88 @@ def get_duplex_posits(ref_seq, ref_nuc):
     """
     nucleotides = ['A', 'T', 'G', 'C']
     start = 0
-    
-    d = {'start_pos': [], 'stop_pos': [], 'ref_duplex': [] }
-    
+
+    d = {'start_pos': [], 'stop_pos': [], 'ref_duplex': []}
+
     while start != len(ref_seq) - 1:
-        
+
         if ref_seq[start] == ref_nuc:
             first_duplex_nuc = ref_seq[start]
             first_duplex_pos = start
-            
+
             # + 1 to get slice from the next nuc
             second_duplex_pos = start + 1
-            for nuc in ref_seq[start + 1 : ]:
+            for nuc in ref_seq[start + 1:]:
                 if nuc in nucleotides:
                     second_duplex_nuc = nuc
                     break
                 else:
                     second_duplex_pos += 1
-                        
+
             d['start_pos'].append(first_duplex_pos)
             d['stop_pos'].append(second_duplex_pos)
             d['ref_duplex'].append(first_duplex_nuc + second_duplex_nuc)
             # incrementing index to move forward the string
             start += 1
-    
+
         else:
             # incrementing anyway
             start += 1
-    
+
     return d
 
-def create_df(duplex_posits, snp_type):
 
+def create_df(duplex_posits, snp_type):
+    """constructs DataFrame using snp_type
+    as columns and duplex_posits as rows
+    """
     df = pd.DataFrame(duplex_posits)
     for snp in snp_type:
         df[snp] = np.nan
-    
+
     df.fillna(inplace=True, value=0)
-    
+
     return df
-    
+
 
 def count_snp(in_file, df, nuc_to_watch):
+    """counts SNPs which occur in the first
+    positions of reference duplexes. SNP is counted 
+    only when the second positions in a duplex in a read
+    remains as in the reference
+    """
     coverage = 0
-    
+
     with open("./input_data/" + in_file) as in_handle:
-        
+
         reads = SimpleFastaParser(in_handle)
         for record in reads:
             coverage += 1
-            
+
             read = record[1]
             for row in df.index:
                 if read[df.loc[row, 'start_pos']] != nuc_to_watch and read[df.loc[row, 'start_pos']] != '-' \
-                and read[df.loc[row, 'stop_pos']] == df.loc[row, 'ref_duplex'][1]:
-                    
+                        and read[df.loc[row, 'stop_pos']] == df.loc[row, 'ref_duplex'][1]:
+
                     snp = read[df.loc[row, 'start_pos']]
-                    
+
                     df.loc[row, snp] += 1
-        
+
     return df, coverage - 1
 
 
 def create_pivot_df(df_snp):
-    
+    """constructs pivot DataFrame
+     grouping data by the duplex variant
+     """
     context_data = df_snp.groupby('ref_duplex').sum()
     context_data.drop(['start_pos', 'stop_pos'], inplace=True, axis=1)
-    
+
     return context_data
 
 
-def save_df(f, df_raw_snp_container, 
-            df_duplex_container, 
+def save_df(f, df_raw_snp_container,
+            df_duplex_container,
             df_perc_container,
             df_cov):
     """saves dataframes into an excel spreadsheet
@@ -149,35 +168,24 @@ def save_df(f, df_raw_snp_container,
         os.mkdir("./output_apobec")
 
     file_name = f.rsplit(".", 1)[0]
-    writer = pd.ExcelWriter("./output_apobec/" + file_name +'.xlsx')
-    
+    writer = pd.ExcelWriter("./output_apobec/" + file_name + '.xlsx')
+
     counter = 0
     for df in df_raw_snp_container:
-        df.to_excel(writer, "raw_count" + str(counter))    
+        df.to_excel(writer, "raw_count" + str(counter))
         counter += 1
-        
+
     for df in df_duplex_container:
         tab_name = df.index[0][0]
         df.to_excel(writer, "nuc_" + tab_name)
-        
+
     for df in df_perc_container:
         tab_name = df.index[0][0]
         df.to_excel(writer, "nuc_" + tab_name + "_percentage")
-    
+
     df_cov.to_excel(writer, "coverage")
 
     writer.save()
-
-    
-    
-def get_input_files_names(path_to_input):
-    
-    fasta_extensions = ["fasta", "fa", "fas"]
-    input_files = os.listdir(path_to_input)
-    input_files = [f for f in input_files if f.rsplit(".", 1)[-1] in fasta_extensions]
-     
-    return input_files
-
 
 
 def create_bar_chart(file_name, df_perc_container, largest_percent):
@@ -189,62 +197,63 @@ def create_bar_chart(file_name, df_perc_container, largest_percent):
     for df in df_perc_container:
 
         pos = [0, 1, 2, 3]
-        width = 0.25 
-        
-        fig, ax = plt.subplots(figsize=(10,5))
-    
+        width = 0.25
+
+        fig, ax = plt.subplots(figsize=(10, 5))
+
         columns = df.columns.tolist()
 
-        plt.bar(pos, 
-                df[columns[0]], 
-                width, 
-                alpha=0.5, 
-                color='darkcyan', 
+        plt.bar(pos,
+                df[columns[0]],
+                width,
+                alpha=0.5,
+                color='darkcyan',
                 edgecolor='black',
                 lw=2,
                 label=columns[0])
 
-        plt.bar([p + width for p in pos], 
+        plt.bar([p + width for p in pos],
                 df[columns[1]],
-                width, 
-                alpha=0.5, 
-                color='silver', 
+                width,
+                alpha=0.5,
+                color='silver',
                 edgecolor='black',
                 lw=2,
                 label=columns[1])
 
-        plt.bar([p + width * 2 for p in pos], 
-                df[columns[2]], 
-                width, 
-                alpha=0.5, 
-                color='sandybrown', 
+        plt.bar([p + width * 2 for p in pos],
+                df[columns[2]],
+                width,
+                alpha=0.5,
+                color='sandybrown',
                 edgecolor='black',
                 lw=2,
                 label=columns[2])
 
         ax.set_ylabel('percent')
         ax.set_xlabel('reference duplex', fontsize=15)
-        ax.set_title("SNP percent at the first position in the duplex context", fontsize=20)
+        ax.set_title(
+            "SNP percent at the first position in the duplex context", fontsize=20)
         ax.set_xticks([p + 1.0 * width for p in pos])
 
         ax.set_xticklabels(df.index)
         plt.legend(loc='upper left', title="SNP type")
         plt.ylim(0, largest_percent + 5)
-    
+
         nuc = df.index[0][0]
-        fig.savefig("./output_apobec/" + file_name.rsplit(".", 1)[0] + "_bars_" + nuc +  ".png") 
+        fig.savefig("./output_apobec/" + file_name.rsplit(".", 1)
+                    [0] + "_bars_" + nuc + ".png")
         # plt.show() # comment to save figure, otherwise it'll save blank file
         plt.close()  # comment the line to show the figure in the jupyter or wherever
 
 
-def _get_current_time():
+def get_current_time():
     """just returns time stamp
     """
     time_stamp = datetime.datetime.fromtimestamp(
         time()).strftime('%Y-%m-%d %H:%M:%S')
-    
-    return time_stamp
 
+    return time_stamp
 
 
 def show_report(total_time, total_files):
@@ -266,37 +275,36 @@ def show_report(total_time, total_files):
     ... job finished at {4}
     ---------------
     
-    """.format(total_files, hours, minutes, int(seconds), _get_current_time()))
- 
+    """.format(total_files, hours, minutes, int(seconds), get_current_time()))
 
 
 def main():
-    
+
     start_time = time()
     file_counter = 0
     nucleotides = ["A", "T", "G", "C"]
-    
+
     if os.path.exists("./input_data"):
         input_files = get_input_files_names("./input_data")
-        
+
         num_files = len(input_files)
         print("""
                ---------------
                job started at {0} ...
                ---------------
-               """.format(_get_current_time()))
-        
+               """.format(get_current_time()))
+
         progress_bar = FloatProgress(min=0, max=num_files)
         display(progress_bar)
-    
+
         for f in input_files:
             df_raw_snp_container = []
             df_duplex_container = []
-            
+
             for nuc in nucleotides:
                 snp_type = nucleotides[:]
                 snp_type.remove(nuc)
-                
+
                 try:
                     ref_seq = get_ref(f)
                 except PermissionError as permerr:
@@ -306,10 +314,10 @@ def main():
                       which doesn't allow it to be processed
                       Now this file is skipped
                        """.format(f))
-                
+
                 duplex_posits = get_duplex_posits(ref_seq, nuc)
                 df = create_df(duplex_posits, snp_type)
-                
+
                 try:
                     df_snp, coverage = count_snp(f, df, nuc)
                 except PermissionError as permerr:
@@ -319,35 +327,35 @@ def main():
                       which doesn't allow it to be processed
                       Now this file is skipped
                        """.format(f))
-                
+
                 df_duplex_in_context = create_pivot_df(df_snp)
                 df_raw_snp_container.append(df_snp)
                 df_duplex_container.append(df_duplex_in_context)
-                
+
             # TODO
             # three loops under should be wrapped as function
             # like : get_summary
-            
+
             total_snpes = 0
             for df in df_duplex_container:
                 total_snpes += df.sum().sum()
-            
+
             df_perc_container = []
             for df in df_duplex_container:
                 df_perc_container.append(df / total_snpes * 100)
-                
+
             largest_percent_in_df = []
             for df in df_perc_container:
                 largest_percent_in_df.append(df.max().max())
             largest_percent = max(largest_percent_in_df)
-            
-            
-            df_cov = pd.DataFrame.from_dict({"coverage": coverage}, orient='index')
+
+            df_cov = pd.DataFrame.from_dict(
+                {"coverage": coverage}, orient='index')
 
             try:
-                save_df(f, 
-                        df_raw_snp_container, 
-                        df_duplex_container, 
+                save_df(f,
+                        df_raw_snp_container,
+                        df_duplex_container,
                         df_perc_container,
                         df_cov)
             except PermissionError as permerr:
@@ -356,26 +364,26 @@ def main():
                       To process the corresponding 'fasta' file, rerun the script
                       now this file is skipped. error: {1}
                       """.format(f, permerr))
-            
-            try:   
+
+            try:
                 create_bar_chart(f, df_perc_container, largest_percent)
             except ValueError as valerr:
                 print(""""
                       plot can't be constructed. 
                       sequences in the input file may be too short. {0}
                       """.format(valerr))
-            
+
             progress_bar.value += 1
             file_counter += 1
-        
+
         finish_time = time()
         total_time = finish_time - start_time
         show_report(total_time, file_counter)
-        
+
     else:
         os.mkdir("./input_data")
         print(
-             """
+            """
         Houston, we have a problem...
         --------
         folder 'input_data' doesn't exist in the current directory 
@@ -385,9 +393,8 @@ def main():
         and run this script again.
         --------
         """
-        )      
-        
+        )
+
+
 if __name__ == "__main__":
     main()
-
-
--- scripts/snp_rate.py
+++ scripts/snp_rate.py
@@ -34,115 +34,126 @@ NOTE: to speed up the exection instead of 'SeqIO.parse()
 low level fasta parser 'SimpleFastaParser' is used 
 """
 
-from Bio.SeqIO.FastaIO import SimpleFastaParser  # low level fast fasta parser
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
-import pandas as pd
-from time import time
 import datetime
-from ipywidgets import FloatProgress
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from Bio.SeqIO.FastaIO import SimpleFastaParser  # low level fast fasta parser
+from time import time
 from IPython.display import display
+from ipywidgets import FloatProgress
 sns.set()
 
 
+def get_input_files_names(path_to_input):
+    """returns list of fasta files
+    """
+    fasta_extensions = ["fasta", "fa", "fas"]
+    input_files = os.listdir(path_to_input)
+    input_files = [f for f in input_files if f.rsplit(
+        ".", 1)[-1] in fasta_extensions]
+
+    return input_files
+
 
 def get_ref(in_file):
-    
+    """returns reference sequence
+    which is the first record in a fasta file
+    """
     with open("./input_data/" + in_file) as in_handle:
         for title, seq in SimpleFastaParser(in_handle):
             ref_seq = seq
             break
-            
+
     return ref_seq
 
 
 def count_snp_per_read(ref_seq, in_file):
     """returns a list of integers:
     - len of the list equals coverage, i.e. num of reads in file
-    - each int value is a num of snp in a read
+    - each int value represents a num of snp in a read
     """
     snpes_per_read = []
-    
+
     with open("./input_data/" + in_file) as in_handle:
         reads = SimpleFastaParser(in_handle)
-        # coverage_counter = 0   
+        # coverage_counter = 0
 
         for record in reads:
             read = record[1]
             snp_counter = 0
             for ind in range(0, len(ref_seq)):
                 if read[ind] != ref_seq[ind] and read[ind] != "-":
-                    if ref_seq[ind] != "-": # skip gaps, insertions are not counted
+                    if ref_seq[ind] != "-":  # skip gaps, insertions are not counted
                         snp_counter += 1
-                        
-            #coverage_counter += 1    
+
+            #coverage_counter += 1
             snpes_per_read.append(snp_counter)
-            
-        
+
         series_name = in_file.rsplit(".", 1)[0]
         snpes_per_read = pd.Series(snpes_per_read, name=series_name)
-        
+
     return snpes_per_read
 
 
-def get_input_files_names(path_to_input):
-    
-    fasta_extensions = ["fasta", "fa", "fas"]
-    input_files = os.listdir(path_to_input)
-    input_files = [f for f in input_files if f.rsplit(".", 1)[-1] in fasta_extensions]
-     
-    return input_files
-
-
-
-def create_strip_plot(df, time_stamp):
-    fig = plt.figure(figsize=(15, 10 ))
-    sns.stripplot(data=df.loc[:, :], jitter=True, size=4)
-    plt.xticks(rotation=90)
-    
-    time_stamp = time_stamp.split(":")
-    time_stamp = "-".join(time_stamp)
-    plt.xticks(fontsize=10)
-    plt.ylabel("number of SNP", fontsize=15)
-    plt.xlabel("input file name", fontsize=15)
-    plt.tight_layout()
-    fig.savefig("./output_apobec/mutation_rate_distribution_" + time_stamp +  "_.png") 
-    # plt.show() # comment to save figure, otherwise it'll save blank file
-    plt.close()  # comment the line to show the figure in the jupyter or wherever
-
-
-
 def get_mode_median(df_raw_count):
-    """calcualte mode and median
+    """calcualtes mode and median
     using raw snp count
     """
     mode = df_raw_count.mode()
-    mode.index = ["mode"] # put in list, otherwise an error "a collection of some kind"
+    # put in list, otherwise an error "a collection of some kind"
+    mode.index = ["mode"]
     median = pd.DataFrame(df_raw_count.median()).T
     median.index = ["median"]
-    
+
     return mode, median
 
 
 def calculate_non_mutated(pivot_df, df_raw_count):
+    """counts zero values in columns of 
+    df_raw_count. Zero values represent reads 
+    without SNPS
+    """
     cols = []
     non_mutated = []
     for col in df_raw_count:
         col_in_list = df_raw_count[col].tolist()
         cols.append(col)
         non_mutated.append(col_in_list.count(0))
- 
-    df_non_mutated = pd.DataFrame([non_mutated], columns=cols) # list is in [] as nested, to get values in a row in df
-    df_non_mutated.rename(index={0: "number of non-mutated reads"}, inplace=True)
+
+    # list is in [] as nested, to get values in a row in df
+    df_non_mutated = pd.DataFrame([non_mutated], columns=cols)
+    df_non_mutated.rename(
+        index={0: "number of non-mutated reads"}, inplace=True)
     pivot_df = pd.concat([pivot_df, df_non_mutated])
-    
+
     # calculating percent of non-mutated reads
     pivot_df.loc['percent non-mutated reads'] = pivot_df.loc["number of non-mutated reads"] / \
-    pivot_df.loc["reads total"] * 100
-    
+        pivot_df.loc["reads total"] * 100
+
     return pivot_df
 
+
+def create_strip_plot(df, time_stamp):
+    """constructs distribution plot
+    using seaborn 'stripplot'
+    """
+    fig = plt.figure(figsize=(15, 10))
+    sns.stripplot(data=df.loc[:, :], jitter=True, size=4)
+    plt.xticks(rotation=90)
+
+    time_stamp = time_stamp.split(":")
+    time_stamp = "-".join(time_stamp)
+    plt.xticks(fontsize=10)
+    plt.ylabel("number of SNPs in a read", fontsize=15)
+    plt.xlabel("input file name", fontsize=15)
+    plt.title("SNP distribution", fontsize=20)
+    plt.tight_layout()
+    fig.savefig("./output_apobec/mutation_rate_distribution_" +
+                time_stamp + "_.png")
+    # plt.show() # comment to save figure, otherwise it'll save blank file
+    plt.close()  # comment the line to show the figure in the jupyter or wherever
 
 
 def save_df(pivot_df, df_raw_count, time_stamp):
@@ -150,34 +161,35 @@ def save_df(pivot_df, df_raw_count, time_stamp):
     """
     if not os.path.exists("./output_apobec"):
         os.mkdir("./output_apobec")
-    
+
     time_stamp = time_stamp.split(":")
     time_stamp = "-".join(time_stamp)
-    
-    writer = pd.ExcelWriter("./output_apobec/mutation_rate_" + time_stamp +'_.xlsx')
-    pivot_df.to_excel(writer, "pivot spreadsheet") 
-    df_raw_count.to_excel(writer, "snp raw count")    
-    
+
+    writer = pd.ExcelWriter(
+        "./output_apobec/mutation_rate_" + time_stamp + '_.xlsx')
+    pivot_df.to_excel(writer, "pivot spreadsheet")
+    df_raw_count.to_excel(writer, "snp raw count")
+
     writer.save()
-    
+
 
 def get_current_time():
-    """just returns time stamp
+    """ returns time stamp
     """
     time_stamp = datetime.datetime.fromtimestamp(
         time()).strftime('%Y-%m-%d %H:%M:%S')
-    
+
     return time_stamp
 
 
 def show_report(total_time, total_files):
-    """prints out  brief report 
+    """prints out brief report 
     """
 
     hours = total_time // 3600
     minutes = (total_time % 3600) // 60
     seconds = total_time % 60
-    
+
     print("""
     
     file processed: {0}
@@ -193,81 +205,84 @@ def show_report(total_time, total_files):
     ---------------
     
     """.format(total_files, hours, minutes, int(seconds), get_current_time()))
- 
 
 
 def main():
-    
+
     start_time = time()
     file_counter = 0
-    
+
     if os.path.exists("./input_data"):
         input_files = get_input_files_names("./input_data")
-        
+
         num_files = len(input_files)
-        
 
         print("""
                ---------------
                job started at {0} ...
                ---------------
                """.format(get_current_time()))
-        
+
         print("collecting snp...")
         progress_bar = FloatProgress(min=0, max=num_files)
         display(progress_bar)
-        
+
         series_container = []
         for f in input_files:
             try:
                 ref_seq = get_ref(f)
                 snpes_per_read = count_snp_per_read(ref_seq, f)
             except PermissionError as permerr:
-                    print("""
+                print("""
                       warning: It seems that the file'{0}' is open in some other 
                       application, like 'Geneious' or whatever,
                       which doesn't allow it to be processed
                       Now this file is skipped
                        """.format(f))
-                
+
             series_container.append(snpes_per_read)
             progress_bar.value += 1
             file_counter += 1
-        
+
         print("snp collected...")
         df_raw_count = pd.DataFrame(series_container).T
-        df_raw_count.drop([0], inplace=True) # drop first raw which is a reference to itself comparison
+        # drop first raw which is a reference to itself comparison
+        df_raw_count.drop([0], inplace=True)
         pivot = df_raw_count.describe()
         mode, median = get_mode_median(df_raw_count)
         pivot_df = pd.concat([pivot, mode, median])
-    
+
         # renaming index values in pivot df
         pivot_df.rename(index={'count': 'reads total',
-                              'mean': 'mean snp per read',
-                              'min': 'min number of snp',
-                              'max': 'max number of snp',
+                               'mean': 'mean snp per read',
+                               'min': 'min number of snp',
+                               'max': 'max number of snp',
                                'mode': 'mode snp',
                                'median': 'median snp'
-                              }, inplace=True)
+                               }, inplace=True)
 
         pivot_df = calculate_non_mutated(pivot_df, df_raw_count).T
-        
+
         time_stamp = get_current_time()
-        create_strip_plot(df_raw_count, time_stamp)
-        print("plot consctructed...")
         
-        df_raw_count = df_raw_count.T #transpose after plotting
+        try:
+            create_strip_plot(df_raw_count, time_stamp)
+            print("plot consctructed...")
+        except:
+            print("something went wrong, plot construction is skipped...")
+
+        df_raw_count = df_raw_count.T  # transpose after plotting
         save_df(pivot_df, df_raw_count, time_stamp)
         print("spreadsheet saved...")
-        
+
         finish_time = time()
         total_time = finish_time - start_time
         show_report(total_time, file_counter)
-        
+
     else:
         os.mkdir("./input_data")
         print(
-             """
+            """
         Houston, we have a problem...
         --------
         folder 'input_data' doesn't exist in the current directory 
@@ -277,9 +292,8 @@ def main():
         and run this script again.
         --------
         """
-        )      
-        
+        )
+
+
 if __name__ == "__main__":
     main()
-        
-    
